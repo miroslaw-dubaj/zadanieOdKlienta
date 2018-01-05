f33d5a4097d0d7929d476dfcdf1fade5b135f1b1
MiroslawDubaj <miroslaw.dubaj@gmail.com>
2018-01-05 08:43:41 +0100

    zOK_Ver_2
    Secound running version - corected validation, added new function splitValidSpit

--- ZadanieOdKlienta.js
+++ ZadanieOdKlienta.js
@@ -1,5 +1,7 @@
 let oldDatabase = 'login=marian12;password=jaromir1,date=2016-12-24;login=janusz422;password=mypassword772,login=witek33;password=mojehaslo23;date=2014-02-54,password=wincenty;login=asdad22;date=2017-12-12';
 
+let globalIndex = 0;
+
 function loginValidate (string) {
     let iCount = 0;
     for (iIndex in string) {
@@ -7,64 +9,61 @@ function loginValidate (string) {
             iCount++;
         }
     }
-    if (iCount >= 2) {
+    if (string.length > 0 && iCount >= 2) {
         return true; }
         return false;
     }
 
 function passValidate (string) {
-    let sTest = string;
     let iCount = 0;
-    for (iIndex in sTest) {
-        if (!isNaN(parseInt(sTest[iIndex]))) {
+    for (iIndex in string) {
+        if (!isNaN(parseInt(string[iIndex]))) {
             iCount++;
         }
     }
-    if (iCount >= 1) {
+    if (string.length > 0 && iCount >= 1) {
         return true; }
         return false;
     }
-
-    function dateValidate (string) {
-        let year = parseInt(string.slice(0,4));
-        let month = parseInt(string.slice(5,7));
-        let day = parseInt(string.slice(8,10));
+    
+function dateValidate (string) {
+    let year = parseInt(string.slice(0,4));
+    let month = parseInt(string.slice(5,7));
+    let day = parseInt(string.slice(8,10));
         if (year >= 2016 && month >=1 && month <= 12 && day >= 1 && day <= 31 && string.length === 10) {
             return true;
         } else return false;
-    }
+   }
 
+    function splitValidSpit(string) {
+       let recordSeparator = string.indexOf("=");
+       let validRecord = string.substring(recordSeparator+1);
+        if (string.indexOf("login") !== -1 && loginValidate(validRecord)) {
+              globalIndex = 0;
+              return validRecord;
+        } else if (string.indexOf("password") !== -1 && passValidate(validRecord)) {
+              globalIndex = 1;
+              return validRecord;
+        } else if (string.indexOf("date") !== -1 && dateValidate(validRecord)) { 
+              globalIndex = 2;
+              return validRecord; 
+        }
+    }
 
 function adaptOldData (database) {
     let splitArray = database.split(',');
     let newOrderArray = [];
     while (splitArray.length > 0) {
-        let string = splitArray.pop()
+        let string = splitArray.pop();
         let reorderedArray = [];
         let temp = string.split(';');
             for (k = 0; k < temp.length; k++) {
-            let stringTemp = temp[k]
-            if (stringTemp.indexOf("login") !== -1) {
-                let recordSeparator = stringTemp.indexOf("=");
-                let validRecord = stringTemp.substring(recordSeparator+1);
-                if (loginValidate(validRecord)) {
-                reorderedArray[0] = (validRecord);
-                } else break;
-            } else if (stringTemp.indexOf("password") !== -1) {
-                let recordSeparator = stringTemp.indexOf("=");
-                let validRecord = stringTemp.substring(recordSeparator+1);
-                if (passValidate(validRecord)) {
-                reorderedArray[1] = (validRecord);
-                } else break;
-            } else if ((stringTemp.indexOf("date") !== -1)) {
-                let recordSeparator = stringTemp.indexOf("=");
-                let validRecord = stringTemp.substring(recordSeparator+1);
-                if (dateValidate(validRecord)) {
-                reorderedArray[2] = (validRecord);
-                } else break;
+            let stringTemp = temp[k];
+            let result = splitValidSpit(stringTemp);
+            reorderedArray[globalIndex] = result;
             }
-        }
-        if (reorderedArray.length > 2) {
+        globalIndex = 0;
+        if (reorderedArray[0] !== undefined > 0 && reorderedArray[1] !== undefined && reorderedArray[2] !== undefined) {
         newOrderArray.unshift(reorderedArray.join(","));
         }
     }
